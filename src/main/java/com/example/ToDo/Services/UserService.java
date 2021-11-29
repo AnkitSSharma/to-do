e0eafe47be032d465296d445b4d1900d24bc4851
Ankit <ankitsharma.0005@gmail.com>
2021-11-29 22:16:40 +0530

    User Services to decide what actions should be performed on incoming data from API's

---
+++ src/main/java/com/example/ToDo/Services/UserService.java
@@ -0,0 +1,39 @@
+package com.example.ToDo.Services;
+
+import com.example.ToDo.Entities.User;
+import com.example.ToDo.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class UserService {
+    @Autowired
+
+    private UserRepository userRepository;
+    public ResponseEntity<String> createUser(User user) {
+
+        if(user.getName()==null||user.getUserName()==null||user.getPassword()==null||user.getEmail()==null){
+            return new ResponseEntity<>(
+                    "Incorrect Inputs",
+                    HttpStatus.BAD_REQUEST);
+        }
+        String unHashedPassword = user.getPassword();
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(unHashedPassword);
+        user.setPassword(hashedPassword);
+        user.setCreated(LocalDateTime.now());
+        userRepository.save(user);
+        return ResponseEntity.ok("User Created Succesfully");
+    }
+
+    public boolean isValid(String name, String password) {
+        User user = userRepository.getByUserName(name);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.matches(password,user.getPassword());
+    }
+}

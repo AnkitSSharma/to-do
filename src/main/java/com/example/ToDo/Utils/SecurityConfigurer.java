c30d4f740d1809e8f739ea6088b3ee7f250b91f1
Ankit <ankitsharma.0005@gmail.com>
2021-11-30 01:39:27 +0530

    Security Configuration set so api can be accessed by user's jwt token, signup and login api are global

--- src/main/java/com/example/ToDo/Utils/SecurityConfigurer.java
+++ src/main/java/com/example/ToDo/Utils/SecurityConfigurer.java
@@ -1,15 +1,48 @@
 package com.example.ToDo.Utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    JwtInterceptor jwtInterceptor;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new MyUserDetailService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/").hasAnyRole()
+                .antMatchers("/user/signup", "/user/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtInterceptor, UsernamePasswordAuthenticationFilter.class);
 
     }
 }

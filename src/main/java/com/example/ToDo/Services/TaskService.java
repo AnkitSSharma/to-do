3f0f9e0fc8779633415e246e2292064689004a31
Ankit <ankitsharma.0005@gmail.com>
2021-11-30 20:58:37 +0530

    Task service created to handle inputs of api to database

---
+++ src/main/java/com/example/ToDo/Services/TaskService.java
@@ -0,0 +1,39 @@
+package com.example.ToDo.Services;
+
+import com.example.ToDo.Entities.SubTask;
+import com.example.ToDo.Entities.Task;
+import com.example.ToDo.Repositories.SubTaskRepository;
+import com.example.ToDo.Repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public ResponseEntity<String> createTask(String userName, Task task) {
+        task.setUserName(userName);
+        task.setCreated(LocalDateTime.now());
+        task.setStatus(Task.TaskStatus.Pending);
+        taskRepository.save(task);
+        return ResponseEntity.ok("Task Created Successfully");
+    }
+
+    public List<Task> getAllTask(String userName, Task.TaskStatus status) {
+        if(status==null){
+            return taskRepository.getAllByUserName(userName);
+        }
+            return taskRepository.getAllByUserNameAndStatus(userName,status);
+
+    }
+
+    public List<Task> getAllByTitle(String userName, String title) {
+        return taskRepository.getByUserNameAndTitleContainsIgnoreCase(userName,title);
+    }
+}

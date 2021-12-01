b475d2f26c8debe3418b3b6c894bb97ffb1df495
Ankit <ankitsharma.0005@gmail.com>
2021-12-01 18:49:14 +0530

    cron job activated

--- src/main/java/com/example/ToDo/ToDoApplication.java
+++ src/main/java/com/example/ToDo/ToDoApplication.java
@@ -1,9 +1,20 @@
 package com.example.ToDo;
 
+import com.example.ToDo.Utils.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.time.*;
 
 @SpringBootApplication
+@EnableScheduling
 public class ToDoApplication {
 
 	public static void main(String[] args) {
